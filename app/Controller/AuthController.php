56f1590f30799c988ebc71ac7bbc0b9f64e5ec83
Luis A. Rubio <luirting@gmail.com>
2018-10-17 07:06:59 -0700

    Change AuthController and routes.
    Missing key in AuthController.php, add router group and deleted debug
    script.

--- app/Controller/AuthController.php
+++ app/Controller/AuthController.php
@@ -11,7 +11,6 @@ namespace App\Controller;
  use App\Helpers\UrlHelper;
  use App\Repositories\UsuarioRepositories;
 
-echo "<script>console.log('klkñklñl');</script>";
 
 class AuthController extends Controller{
   private $config;
@@ -19,19 +18,22 @@ class AuthController extends Controller{
 
   public function __construct()
   {
-    echo "<script>console.log('consteucut');</script>";
     parent::__construct();
     if(Auth::isLoggedIn()){
       UrlHelper::redirect();
     }
     $this->config=ServicesContainer::getConfig();
-    $this->$usuarioRep=new UsuarioRepositories();
+    $this->usuarioRep=new UsuarioRepositories();
   }
 
-  public function getindex(){
-    $nrand=rand(1,5);
-    return $this->render('auth/index.twig',[
-      'title'=>'Autentificacion','fondo'=>$nrand, 'company_name'=>$this->config['company_name']
-    ]);
+  public function getindex()
+  {
+      $nrand = rand(1, 5);
+      return $this->render('auth/index.twig', [
+          'title' => 'Autentificacion',
+          'fondo' => $nrand,
+          'company_name' => $this->config['company_name']
+      ]);
+  }
 
 }
